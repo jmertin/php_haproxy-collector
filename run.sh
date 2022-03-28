696f7c8d40aca0c46cab12c23fceb474c2422a9e
Joerg Mertin <joerg.mertin@ca.com>
2022-03-28 15:11:20 +0200

    start file from CLI when not running inside docker

--- run.sh
+++ run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #
-#
+# These entries can be used on docker-variables!
 export hapver=1.7
 export hapuser="admin"
-export happass="PA55w0rd"
-export hapurl="https://pcm.solsys.org/haproxy_stats;csv"
+export happass="xxxxxxxx"
+export hapurl="https://haproxy-url.domain.tld/haproxy_stats;csv"
 export happort=2000
 export self_signed=true
 export send_all=
 
 export hapdebug= # Set to true for activating debugging
+export hapstats= # Set to true for activating stats in console
 
-export apmia_url="http://calypso.solsys.org/apm/metricFeed"
+export apmia_url="http://apmia-url.domain.tld/apm/metricFeed"
 export apmia_port=8080
 export poll_time=7
 
 
-
+# Run actual script.
 php ./hacollector.php
