553e5851c76086a1c0dfe1808b80722173fc4891
Joerg Mertin <joerg.mertin@ca.com>
2022-04-07 15:12:13 +0200

    "exported" poller functions to respectively poller_csv and poller_json

--- hacollector.php
+++ hacollector.php
@@ -5,13 +5,17 @@
    ==============================================================================================
 */
 
+// Load misc functions.
+include_once('./include/misc.inc');
+
 // Load configuration
-include('./conf/hacollector.conf'); // will load the config.inc from the same directory.
+include_once('./conf/hacollector.conf'); // will load the config.inc from the same directory.
+
 
 // Implemnent file locking. We don't want this script to run more than once.
 // Note - the Lockfile has a name composed of the script-name and the haproxy
 // csv URL as we assume we'll be polling this HA Proxy server only once.
-$filename = preg_replace('/\.\//', '', $_SERVER['PHP_SELF']) . "." . hash('adler32', "$csvurl") . "..LCK";
+$filename = preg_replace('/\.\//', '', $_SERVER['PHP_SELF']) . "." . hash('adler32', "$pollurl") . "..LCK";
 $fp = fopen( $filename,"w"); // open it for WRITING ("w")
 if (flock($fp, LOCK_EX)) {
     file_put_contents($filename, posix_getppid());
@@ -22,9 +26,6 @@ if (flock($fp, LOCK_EX)) {
     print "Could not lock $filename!\n";
 }
 
-// Load the haproxy version file - configuration for what to poll and type definitions.
-include("./conf/haproxy_" . $haproxy_version . ".inc"); // will load haproxy file
-
 // Showing current config
 print "\n";
 print "*** HAProxy stats to APMIA RESTFul collector started\n";
@@ -32,7 +33,7 @@ print " => The following configuration is set (conf/hacollector.conf)\n";
 print " =============================================================\n";
 
 print " => HA Proxy version: $haproxy_version \n";
-print "    - haproxy URL: $csvurl \n";
+print "    - haproxy URL: $pollurl \n";
 print "    - haproxy port: $haproxy_port \n";
 if (strlen($username) > 0) {
     print "    - haproxy access via user: $username \n";
@@ -48,233 +49,39 @@ print "    - Debug flag: $DEBUG \n";
 print "    - Statistics flag: $STATS \n";
 print " => Lockfile $filename created.\n";
 
-
-// The URL is always an issue. Make sure it is currect.
-function check_url($text, $return_var, $description){
-       
-    // define the answer array and empty it
-    $answer = array();
-    $answer['error_msg'] = '';
-    
-    // URL needs to be valid, not 0
-    if (strlen($text) > 0) {
-        if (preg_match("/^http/i",$text)) {
-            if (filter_var($text, FILTER_VALIDATE_URL)  === FALSE) {
-                $answer["$return_var"] = htmlentities($text, ENT_QUOTES, 'UTF-8');
-                $answer['error_msg'] = "*** FATAL: URL format for $description \"" . $answer["$return_var"] . "\" invalid!";
-            } else {
-                $answer["$return_var"] = htmlentities($text, ENT_QUOTES, 'UTF-8');
-            }
-        }           
-    } else {
-        // If Data was empty - it won't hurt here either
-        $answer["$return_var"] = "";
-        $answer['error_msg'] = "*** FATAL: Valid URL for $description required!";
-    }
-
-    // Return what we found out.
-    return($answer);
-
-} // function check_url
-
-
 // tick use required
 declare(ticks = 1);
-
-// signal handler function
-function sig_handler($signo) {
-    global $filename;
-
-    switch ($signo) {
-    case SIGTERM:
-        print "Received $signo - cleaning up!\n";
-        // handle shutdown tasks
-        if (file_exists($filename)) {
-            unlink($filename);
-        }
-        exit;
-        break;
-    case SIGINT:
-        print "Received $signo - cleaning up!\n";
-        // handle restart tasks
-        if (file_exists($filename)) {
-            unlink($filename);
-        }
-        exit;
-        break;
-    case SIGUSR1:
-        print "Received $signo - Rereading configuration!\n";
-        // Load configuration
-        include('./conf/hacollector.conf'); // will load the config.inc from the same directory.
-        // Load the haproxy version file - configuration for what to poll and type definitions.
-        include("./conf/haproxy_" . $haproxy_version . ".inc"); // will load haproxy file
-        // handle restart tasks
-    default:
-        // handle all other signals
-        print "Received $signo - don't know what to do!";
-    }
-
-} // function sig_handler
-
-// Actual function, doing the HAProxy polling etc.
-function poller() {
-    global $haproxy_version, $haproxy_port, $DEBUG, $username, $password, $csvurl, $self_signed, $apmia_url, $apmia_port, $poll_time, $apmia_send, $apmia_des, $apmia_type, $send_all, $STATS, $filename;
-
-    // Checking the provided URL for HAPRoxy
-    $url_check = check_url($csvurl, "clean", "HAProxy");
-    if (strlen($url_check['error_msg']) > 1) {
-        // we have an issue here. Error message should not exist.
-        print "{$url_check['error_msg']} - ";
-        unlink($filename);
-        posix_kill(posix_getpid(), SIGTERM);
-    }
-
-    // Checking the provided for APMIA
-    $url_check = check_url($apmia_url, "clean", "APMIA");
-    if (strlen($url_check['error_msg']) > 1) {
-        // we have an issue here. Error message should not exist.
-        print "{$url_check['error_msg']} - ";
-        unlink($filename);
-        posix_kill(posix_getpid(), SIGTERM);
-    }
-
-    // Check port for apmia
-    if (($haproxy_port < 65536) && ($haproxy_port > 0)) {
-            $port['check'] = "valid";
-        } else {
-            print "*** FATAL: Port $haproxy_port for APMIA invalid. - ";
-            unlink($filename);
-            posix_kill(posix_getpid(), SIGTERM);
-        }
-
-    // Check port for apmia
-    if (($apmia_port < 65536) && ($apmia_port > 0)) {
-            $port['check'] = "valid";
-        } else {
-            print "*** FATAL: Port $apmia_port for APMIA invalid. - ";
-            unlink($filename);
-            posix_kill(posix_getpid(), SIGTERM);
-        }
-    
-    // Init ch
-    $ch = curl_init($csvurl);
-
-    // If curl debugging is required, uncomment the following line.
-    curl_setopt($ch, CURLOPT_VERBOSE, $DEBUG);
-
-    // In case HAProxy runs on its own port, set the port here.
-    curl_setopt($ch, CURLOPT_PORT , $haproxy_port);
-
-    if ($self_signed) {
-        // In case SSL key is self signed, activate both the lines below
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    }
-
-    if (strlen($username) > 0) {
-        // Specify the username and password using the CURLOPT_USERPWD option.
-        // This will cause curl to generate a basic authentication header
-        curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
-    }
-    // Tell cURL to return the output as a string instead of dumping it to the browser
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // Execute the request
-    $csvcont = curl_exec($ch);
-
-    //Check for errors.
-    if (curl_errno($ch)) {
-        // If an error occured, throw an Exception.
-        throw new Exception(curl_error($ch));
-    }
-
-    // Close data collection from haproxy.
-    curl_close($ch);
-
-    // Initialize what we need
-    $header = NULL; // header array to be used
-    $assoc_data = array(); // associative array containing all results
-
-    // the row always has a "# " at the beginning. strip it out.
-    $csvcont = preg_replace('/^#\ /', '', $csvcont);
-
-    $DEBUG && print  " 1 => Return CSV Content : \n $csvcont \n\n";
-
-    // it gets tricky here. The web-server returns only a string, we need to separate each line
-    $data = str_getcsv($csvcont, "\n");
-
-    $DEBUG && print " 2 => Return Data array : \n" . print_r($data);
-
-    // Cycle through all entries in the previous found data and create an
-    // associative array for the data source.
-    foreach ($data as &$row) {
-        // Dump array content into new array
-        $row_arr = str_getcsv($row, ",");
-
-        // If we have no header line, 1st line is header!
-        if(!$header) {
-            $header = $row_arr;
-        } else {
-            // All other lines are regular entries
-            $assoc_data[] = array_combine($header, $row_arr);
-        }
-    }
-
-    $DEBUG && print " 3 => Return Associative array : \n" . print_r($assoc_data);
-
-    // #1 Cycle through associative array
-    foreach ($assoc_data as &$output) {
-        $coma = "";
-        $line = "";
-        // #2 Extract key/values out of master array
-        foreach ($output as $key => $value) {
-
-            // Sanitize data somehow
-            if ($send_all) {
-                if ($value == "") {$value = 0; }
-            }
-
-            if (strlen($value) > 0) {
-                if ($apmia_send["{$key}"]) {
-                    // Key will be used to extract the values out of the apmia_des array
-                    $line .=  $coma . "{\"type\" : \"" .
-                          $apmia_des["{$key}"] . "\", \"name\" : \"haproxy|" .
-                          $output['pxname'] . "|" .
-                          $output['svname'] .
-                          $apmia_type["{$key}"] . "\", \"value\" : \"" .
-                          $value . "\"}";
-                    $coma=",";
-                }
-            }
-        } // Foeach loop.
-
-        // This is the entire line that will be sent as json payload to the APMIA.
-        $tosend = "{ \"metrics\" : [" . $line . "] }";
-
-        $xch = curl_init( $apmia_url );
-        curl_setopt($xch, CURLOPT_PORT , $apmia_port);
-        // prep data to send to APMIA
-        curl_setopt( $xch, CURLOPT_POSTFIELDS, $tosend );
-        curl_setopt( $xch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        curl_setopt( $xch, CURLOPT_RETURNTRANSFER, true );
-        $result = curl_exec($xch);
-
-        //Check for errors.
-        if (curl_errno($xch)) {
-            // If an error occured, throw an Exception.
-            throw new Exception(curl_error($xch));
-        }
-        $DEBUG && print "$result \n";
-        $STATS && print " > Polled: " . gmstrftime("%Y-%m-%d %T %Z") . " {$output['pxname']}|{$output['svname']} Agent answer: $result \n";
-        // print "\n\n $tosend\n\n"; // Payload in case
-        curl_close($xch);
-    } // foreach loop #2
-}// Foreach loop #1
 
 // Actual code runs here :)
 while (true) {
     // Actually run the code
-    poller();
+    if (endsWith("$pollurl", 'csv')) { 
+        // Load the haproxy version file - configuration for the version is in hacollector.conf
+        include_once("./conf/haproxy_" . $haproxy_version . ".inc"); // will load haproxy file
+
+        // Load csv poller
+        include_once('./include/poller_csv.inc'); // will load the config.inc from the same directory.
+
+        // CSV poller requires static configuration
+        poller_csv();
+
+    } elseif (endsWith("$pollurl", 'json')) {
+        // Load json poller
+        include_once('./conf/haproxy_json.conf'); 
+
+        // Load json poller
+        include_once('./include/poller_json.inc'); 
+
+        // JSon poller does not require static configuration - as it
+        // has the type definitions and field names included in json
+        // string.
+        poller_json();
+        
+    } else {
+        print "*** Wrong target URL format! \n";
+        unlink($filename);
+        posix_kill(posix_getpid(), SIGTERM);
+    }
     // setup signal handlers - or else we cannot interrupt.
     pcntl_signal(SIGTERM, "sig_handler");
     pcntl_signal(SIGINT,  "sig_handler");
